cd9cd4312832f6e16fdaf33b1d8c2719d8fd512e
Laureni Wilkinson <las348@nyu.edu>
2021-04-10 14:45:43 -0400

    implementing redux

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProductDetails } from '../actions/productActions'
+
 
 const ProductScreen = ({ match }) => {
-   const [product, setProduct] = useState({})
+    const dispatch = useDispatch()
 
-   useEffect(() => {
-    const fetchProduct = async () => {
-        const { data } = await axios.get(`/api/products/${match.params.id}`)
+    const productDetails = useSelector(state => state.productDetails)
+    const { loading, error, product } = productDetails
 
-        setProduct(data)
-    }
+    useEffect(() => {
+        dispatch(listProductDetails(match.params.id))
+    }, [dispatch, match])
 
-    fetchProduct()
- }, [match])
 
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>Go Back</Link>
-            <Row>
+            {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
+                <Row>
                 <Col md={6}>
                     <Image src={product.image} alt={product.name} fluid />
                 </Col>
@@ -30,9 +33,9 @@ const ProductScreen = ({ match }) => {
                             <h3>{product.name}</h3>
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            <Rating 
-                            value={product.rating} 
-                            text={`${product.numReviews} reviews`}
+                            <Rating
+                                value={product.rating}
+                                text={`${product.numReviews} reviews`}
                             />
                         </ListGroup.Item>
                         <ListGroup.Item>
@@ -46,40 +49,41 @@ const ProductScreen = ({ match }) => {
                 <Col md={3}>
                     <Card>
                         <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                           <Row>
-                               <Col>
-                                    Price:
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        Price:
                                </Col>
-                               <Col>
-                                    <strong>${product.price}</strong>
-                               </Col>
-                           </Row>
-                        </ListGroup.Item>
+                                    <Col>
+                                        <strong>${product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
 
-                        <ListGroup.Item>
-                           <Row>
-                               <Col>
-                                    Status:
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        Status:
                                </Col>
-                               <Col>
-                                   {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-                               </Col>
-                           </Row>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                           <Button 
-                           className='btn-block' 
-                           type='button' 
-                           disabled={product.countInStock === 0}
-                           >
-                               Add To Cart
+                                    <Col>
+                                        {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button
+                                    className='btn-block'
+                                    type='button'
+                                    disabled={product.countInStock === 0}
+                                >
+                                    Add To Cart
                            </Button>
-                        </ListGroup.Item>
+                            </ListGroup.Item>
                         </ListGroup>
                     </Card>
                 </Col>
             </Row>
+            )}
         </>
     )
 }
