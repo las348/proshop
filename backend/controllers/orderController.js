7bb02d219251f8bbe51928c1494a1ba49c5962e1
Laureni Wilkinson <las348@nyu.edu>
2021-04-25 13:58:49 -0400

    last of admin screens- deliver

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -77,7 +77,25 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
         res.status(404)
         throw new Error('Order not found')
     }
+})
 
+// @desc    Update order to delivered
+// @route   GET /api/orders/:id/deliver
+// @access  Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if (order) {
+        order.isDelivered = true
+        order.deliveredAt = Date.now()
+
+        const updatedOrder = await order.save()
+
+        res.json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
 })
 
 // @desc    Get logged in user orders
@@ -97,4 +115,11 @@ const getOrders = asyncHandler(async (req, res) => {
 })
 
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders, getOrders }
+export { 
+    addOrderItems, 
+    getOrderById, 
+    updateOrderToPaid, 
+    updateOrderToDelivered,
+    getMyOrders, 
+    getOrders 
+}
