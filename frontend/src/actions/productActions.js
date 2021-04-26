37c37cff06029ed520bbaf59dba357b8c5b18c4d
Laureni Wilkinson <las348@nyu.edu>
2021-04-26 16:05:51 -0400

    reviews and product search

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-import { 
-    PRODUCT_LIST_REQUEST, 
-    PRODUCT_LIST_SUCCESS, 
-    PRODUCT_LIST_FAIL, 
+import {
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL,
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
     PRODUCT_DETAILS_FAIL,
@@ -15,13 +15,17 @@ import {
     PRODUCT_UPDATE_REQUEST,
     PRODUCT_UPDATE_SUCCESS,
     PRODUCT_UPDATE_FAIL,
+    PRODUCT_CREATE_REVIEW_REQUEST,
+    PRODUCT_CREATE_REVIEW_SUCCESS,
+    PRODUCT_CREATE_REVIEW_FAIL
 } from '../constants/productConstants'
+import { logout } from './userActions'
 
-export const listProducts = () => async (dispatch) => {
+export const listProducts = (keyword = '') => async (dispatch) => {
     try {
-        dispatch({ type: PRODUCT_LIST_REQUEST})
+        dispatch({ type: PRODUCT_LIST_REQUEST })
 
-        const { data } = await axios.get('/api/products')
+        const { data } = await axios.get(`/api/products?keyword=${keyword}`)
 
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
@@ -30,17 +34,17 @@ export const listProducts = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_LIST_FAIL,
-            payload: 
-            error.response && error.response.data.message  
-            ? error.response.data.message
-            : error.message
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message
         })
     }
 }
 
 export const listProductDetails = (id) => async (dispatch) => {
     try {
-        dispatch({ type: PRODUCT_DETAILS_REQUEST})
+        dispatch({ type: PRODUCT_DETAILS_REQUEST })
 
         const { data } = await axios.get(`/api/products/${id}`)
 
@@ -51,10 +55,10 @@ export const listProductDetails = (id) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_DETAILS_FAIL,
-            payload: 
-            error.response && error.response.data.message  
-            ? error.response.data.message
-            : error.message
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message
         })
     }
 }
@@ -152,5 +156,41 @@ export const updateProduct = (product) => async (dispatch, getState) => {
                     ? error.response.data.message
                     : error.message
         })
+    }
+}
+
+export const createProductReview = (productId, review) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        await axios.post(`/api/products/${productId}/reviews`, review, config)
+
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_SUCCESS
+        })
+
+    } catch (error) {
+        const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      if (message === 'Not authorized, token failed') {
+        dispatch(logout())
+      }
+      dispatch({
+        type: PRODUCT_CREATE_REVIEW_FAIL,
+        payload: message,
+      })
     }
 }
