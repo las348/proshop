4694241567cf2ccd3a2c0559363faf82751bd99a
Laureni Wilkinson <las348@nyu.edu>
2021-04-28 17:25:26 -0400

    getting ready to deploy

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,9 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useUnifiedTopology: true,
-            useNewURLParser: true,
+            useNewUrlParser: true,
             useCreateIndex: true,
-        })
+          })
 
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {
